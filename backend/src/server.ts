12492961bf546bb4a398a66da807baf7c61daf19
Renato Moutinho Silva <renato.moutinho@gmail.com>
2020-07-14 19:32:09 -0300

    Added cors library to backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -11,10 +12,11 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
-
 app.use(routes);
+
 app.use(
   (err: AppError, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
