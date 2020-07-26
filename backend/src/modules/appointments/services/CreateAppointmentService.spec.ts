910191ffa442b028bf414364d68c246d769b6ac9
Renato Moutinho Silva <renato.moutinho@gmail.com>
2020-07-25 21:21:09 -0300

    CreateUserService tests added

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,6 @@
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
+import AppError from '@Shared/errors/AppError';
 
 describe('CreateAppointment', () => {
 
@@ -17,6 +18,25 @@ describe('CreateAppointment', () => {
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe(provider_id);
 
-  })
+  });
 
-})
+  it('should not be able to schedule an appointment for the same date/time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const provider_id = '123123123';
+    const date = new Date();
+
+    await createAppointment.execute({
+      date,
+      provider_id
+    })
+
+    expect(createAppointment.execute({
+      date,
+      provider_id
+    })).rejects.toBeInstanceOf(AppError)
+
+  });
+
+});
